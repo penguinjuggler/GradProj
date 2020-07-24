7e366fb7bb095bce6f1a0d0cb0a875db2a6d85d1
Chris Polis <cmpolis@gmail.com>
2020-07-23 21:26:53 -0700

    refactored material definitions, added radius input and sample calculation

--- GP-JS.js
+++ GP-JS.js
@@ -1,29 +1,64 @@
 class Material {
-    constructor(name, elasticity) {
+    constructor(name, elasticity, foo) {
         this.name = name;
         this.elasticity = elasticity;
+        this.foo = foo;
     }
+}
+
+const materials = {
+  'Aluminum': new Material('Aluminum', 1, 4),
+  'Steel': new Material('Steel', 2, 5),
+};
+
+function calculateFoo(elasticity, radius) {
+  return elasticity * radius;
 }
 
 $(document).ready(function() {
     var materials_select = document.getElementById("materials");
-    var materials_options = ['Aluminum', 'Steel', 'Copper'];
     var material_objs = {};
 
-    for(var i = 0; i < materials_options.length; i++) {
-        var material = new Material(materials_options[i], i);
-        material_objs[materials_options[i]] = material;
+    // Setup material input
+    for (let materialKey in materials) {
+        var material = materials[materialKey];
 
-        var opt = materials_options[i];
         var el = document.createElement("option");
-        el.textContent = opt;
-        el.value = opt;
+        el.textContent = material.name;
+        el.value = material.name;
         materials_select.appendChild(el);
     }
+
+    // Setup units input...
+    //
+    /*
+     *
+     * ﻿
+     * $('#CrossSectionRadius')
+     * S.fn.init [input#CrossSectionRadius]
+     * const someElement = $('#CrossSectionRadius')
+     * undefined
+     * someElement
+     * S.fn.init [input#CrossSectionRadius]
+     * someElement.attr('src', 'foo.svg')
+     * S.fn.init [input#CrossSectionRadius]
+     */
+    
+
     $("#submit1").click(function(e) {
         var text = $("#materials option:selected").text();
-        alert(material_objs[text].elasticity);
-    });
+        const material = materials[text]; 
 
+        // Read cross section radius
+        const radiusInput = $('#CrossSectionRadius');
+        const radiusValue = parseFloat(radiusInput.val());
+
+        // Calculate foo
+        alert(calculateFoo(material.elasticity, radiusValue));
+        
+        // Calculate baz
+    });
 });
+
+
 
