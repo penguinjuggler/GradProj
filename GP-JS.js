735e29757fe8bb3175410811cb21e54d37b64140
Jacob M <penguinjuggler@gmail.com>
2020-07-27 10:56:31 -0700

    Circle / Rect radio buttons added and integrated. Footer added

--- GP-JS.js
+++ GP-JS.js
@@ -37,15 +37,6 @@ function calculateFoo(elasticity, radius) {
   return elasticity * radius;
 }
 
-// Scroll to top button
-/*var MyScrollUp = document.getElementById("ScrolltoTop");
-
-// When the user clicks on the button, scroll to the top of the document
-function topFunction() {
-	document.body.scrollTop = 0;
-	document.documentElement.scrollTop = 0;
-}
-*/
 $(document).ready(function() {
 	
     var materials_select = document.getElementById("MaterialSelect");
@@ -73,7 +64,7 @@ $(document).ready(function() {
     }
 
 	// Sets up Value inputs for Cross Section and Beam Length, 
-	$("#CrossSectionRadius, #BeamLength").attr({
+	$("#Radius, #BeamLength, #Height, #Width").attr({
 		type:'number',
 		min:'1',
 		max:'1000000000',
@@ -81,7 +72,7 @@ $(document).ready(function() {
 		value:'10',
 		step:'1',
 		oninput:"this.value=this.value.slice(0,this.maxLength)",
-		style:'width:100%',
+		//style:'width:80%',
 		required:'true'
 	})
 	
@@ -125,7 +116,7 @@ $(document).ready(function() {
 
 		
         // Calculate foo
-		alert(calculateFoo(material.elasticity, radiusValue));
+		//alert(calculateFoo(material.elasticity, radiusValue));
 
 		// Will change BeamPicture based on final direction of forces
 		// Need to set BeamPicNum above
@@ -133,30 +124,50 @@ $(document).ready(function() {
 		var BeamChange = $("#BeamPic");
 		BeamChange.attr('src', 'BeamPic' + BeamPicNum + '.png');
         */
-		
 			
     });
 	
+	// Sets input options for a radius or rectangle values depending on radio input
+	$("input[type=radio][name=CircleOrRect]").change(function() {
+		if (this.value == 'Rect') {
+			$("#RectDim").removeAttr("hidden");
+			$("#CircleDim").attr("hidden","true");
+		} else if (this.value == 'Circle') {
+			$("#CircleDim").removeAttr("hidden");
+			$("#RectDim").attr("hidden","true");
+		}
+	})
+
+	let ChangeTriggers = $("#MaterialSelect, #UnitSelect, #BeamLength, #CircleDim, #RectDim, input[type=radio][name=CircleOrRect]");
 	
 	// Updates Youngs Modulus on change of Material and Unit Choice
-	$("#MaterialSelect, #UnitSelect, #BeamLength").change(function() {
+	// Also updates "stress results" with arbitrary calculation, to be updated later
+	ChangeTriggers.change(function() {
 		var text1 = $("#MaterialSelect option:selected").text();
 		var text2 = $("#UnitSelect option:selected").text();
 		var elas = parseInt(materials[text1].elasticity * UnitChoice[text2].pressureMult);
 		$("#ElasticityDisplay").text(elas + UnitChoice[text2].pressureText);
 		$("#UnitDisplay").text(UnitChoice[text2].unitList);
 		
-		//Testing realtime output with beamlength as an input
+		// Testing realtime output with beamlength as an input
 		var text3 = elas* $("#BeamLength").val();
 		$("#Output01").text(text3);
 		
+		// Material and Geometry Values
+		var rad = $("#CrossSectionRadius").val();
+		var len = $("#BeamLength").val();
+		
+		// Force and Moment Values
+		var fX = $("#fX").val();
+		var fY = $("#fY").val();
+		var fZ = $("#fZ").val();
+		var mZ = $("#mZ").val();
+		
+		
+		
 	});
 	
-	
-	// Updates Radius or Height/Width request on Circular or Rectangular radio select
-	
-	
-	
+
 
 });
 
