b363f900bdff9da6df76d6c8c6d09a54b8fdc07b
Jacob M <penguinjuggler@gmail.com>
2020-07-28 18:23:50 -0700

    Cross Section, NormZ cleaned up, Loads cleaned, Units fixed
    Cross Section Area pic added, table shuffled around.

--- GP-JS.js
+++ GP-JS.js
@@ -23,62 +23,55 @@ class Units {
 
 // Setup units - multiplier between Metric and Imperial
 const UnitChoice = {
-	'Metric': new Units('Metric','m',1,'GPa',1, 'N',1,'Nm',1,'m / GPa / kgs'),
-	'Imperial': new Units('Imperial','in', 39.37,'kpsi', 145.038, 'lbf',0.225,'in-lb',8.85,'inch / kpsi / lbs'),
+	'Metric': new Units('Metric','m',1,'Pa',1, 'N',1,'Nm',1,'m / Pa / N / Nm'),
+	'Imperial': new Units('Imperial','in', 39.37,'psi', 145.038, 'lbf',0.225,'in-lb',8.85,'inch / psi / lbf / in-lb'),
 };
 
 // Setup Materials and their Young's Modulus of Elasticities (GPa), ultimate tensile strength (MPa), and other material properties
 const materials = {
-  'Aluminum': new Material('Aluminum', 69, 110),
-  'Steel': new Material('Steel', 180, 860),
-  'Copper': new Material('Copper', 117, 220),
+  'Aluminum': new Material('Aluminum', 69*Math.pow(10,9), 110),
+  'Steel': new Material('Steel', 180*Math.pow(10,9), 860),
+  'Copper': new Material('Copper', 117*Math.pow(10,9), 220),
 };
 
 var BeamPicNum;
 
-
+/*
 function calculateFoo(elasticity, radius) {
   return elasticity * radius;
-}
+}*/
 
 $(document).ready(function() {
-	
-    var materials_select = document.getElementById("MaterialSelect");
-	//var materials_Modulus = document.getElementById("YM_Display");
-	var unit_select = document.getElementById("UnitSelect");
-	var CrossSectionArea;
 	
 	
 	// Setup Unit input
     for (let unitKey in UnitChoice) {
         var unit = UnitChoice[unitKey];
-
         var el = document.createElement("option");
         el.textContent = unit.name;
         el.value = unit.name;
-        unit_select.appendChild(el);
+        $("#UnitSelect").append(el);
     }
 	
     // Setup material input
     for (let materialKey in materials) {
         var material = materials[materialKey];
-
         var el = document.createElement("option");
         el.textContent = material.name;
         el.value = material.name;
-        materials_select.appendChild(el);
+        $("#MaterialSelect").append(el);
     }
 
 	// Sets up Value inputs for Cross Section and Beam Length, 
 	$("#Radius, #BeamLength, #Height, #Width").attr({
 		type:'number',
-		min:'1',
+		min:'0.001',
 		max:'1000000000',
 		maxLength:'8',
-		value:'10',
+		//value:'10',
 		step:'.001',
 		oninput:"this.value=this.value.slice(0,this.maxLength)",
-		style:'width:6em',
+		style:'width:6em; text-align:right',
 		required:'true'
 	})
 	
@@ -111,7 +104,8 @@ $(document).ready(function() {
      */
     
 
-    $("#submit1").click(function(e) {
+    /*	Will likely delete this section, since using autoupdate feature
+	$("#submit1").click(function(e) {
         var text = $("#MaterialSelect option:selected").text();
         const material = materials[text]; 
 
@@ -125,12 +119,11 @@ $(document).ready(function() {
 
 		// Will change BeamPicture based on final direction of forces
 		// Need to set BeamPicNum above
-		/*
+		
 		var BeamChange = $("#BeamPic");
 		BeamChange.attr('src', 'BeamPic' + BeamPicNum + '.png');
-        */
-			
-    });
+        	
+    });*/
 	
 	// Sets input options for a radius or rectangle values depending on radio input
 	$("input[type=radio][name=CircleOrRect]").change(function() {
@@ -143,15 +136,17 @@ $(document).ready(function() {
 		}
 	})
 
-	let ChangeTriggers = $("#MaterialSelect, #UnitSelect, #BeamLength, #CircleDim, #RectDim, input[type=radio][name=CircleOrRect], #fX, #fY, #fZ, #mX, #mY, #mZ");
+	let ChangeTriggers = $("#MaterialSelect, #UnitSelect, #BeamLength, #CircleDim, #RectDim, \
+							input[type=radio][name=CircleOrRect], #fX, #fY, #fZ, #mX, #mY, #mZ");
 	
 	// Updates Youngs Modulus on change of Material and Unit Choice
 	// Also updates "stress results" with arbitrary calculation, to be updated later
 	ChangeTriggers.change(function() {
 		var text1 = $("#MaterialSelect option:selected").text();
 		var text2 = $("#UnitSelect option:selected").text();
+		
 		var elas = parseInt(materials[text1].elasticity * UnitChoice[text2].pressureMult);
-		$("#ElasticityDisplay").text(elas + " " + UnitChoice[text2].pressureText);
+		$("#ElasticityDisplay").text(elas.toExponential() + " " + UnitChoice[text2].pressureText);
 		$("#UnitDisplay").text(UnitChoice[text2].unitList);
 		$("#ForceUnits").text(UnitChoice[text2].forceText);
 		$("#MomentUnits").text(UnitChoice[text2].momentText);
@@ -173,13 +168,14 @@ $(document).ready(function() {
 		
 		var Ix, Iy, Iz;
 		var normZ1, normZ2, normZ;
-		var mX_tot, halfy;
+		var mX_tran, halfy, CrossSectionArea;
 		
 		var RadioInput = $("input[type=radio][name=CircleOrRect]:checked").val();
 		if (RadioInput == 'Rect') {
 			// No twisting with rectangular cross section
 			$("#mZ").val('0');
 			$("#mZ").attr('disabled','disabled');
+			$("#CrossSectionPic").attr('src','RectCrossSection.png');
 			CrossSectionArea = (h*w);
 			Ix = (w*Math.pow(h,3)/12); // I = (bh^3)/12
 			Iy = (h*Math.pow(w,3)/12); // I = (hb^3)/12
@@ -188,6 +184,7 @@ $(document).ready(function() {
 		} else if (RadioInput == 'Circle') {
 			// Allows twisting
 			$("#mZ").removeAttr('disabled');
+			$("#CrossSectionPic").attr('src','CircleCrossSection.png');
 			CrossSectionArea = (Math.PI * Math.pow(r,2));
 			Ix = (Math.pow(r,4)*Math.PI/4); // I = (pi*r^4)/4
 			Iy = Ix;
@@ -202,16 +199,18 @@ $(document).ready(function() {
 		$("#AreaOutput").html(CrossSectionArea.toFixed(3) + " " + UnitChoice[text2].lengthText + "<sup>2</sup>");
 		
 		// Normal Stress in Z = P/A - (Mx1 + Mx2)y/I
-		normZ1 = (fZ/CrossSectionArea); // Normal Stress in Z = P/A = Force in Z / Cross Section Area
-		mX_tot = -fY*len + mX; 			// Moment X Total = Force in Y * length of beam + Moment X at end		
-		normZ2 = (mX_tot*halfy)/Ix; 	// normal stress = -M*y/I
-		normZ = normZ1 + normZ2;
+		normZ1 = (fZ/CrossSectionArea); 	// Normal Stress in Z = Fz/A
+		mX_tran = -fY*len; 					// Moment X = Force in Y * length of beam		
+		normZ2 = (mX_tran*halfy)/Ix; 		// Normal stress = -M*y/I due to fY
+		normZ3 = (mX*halfy)/Ix;				// Normal stress = -M*y/I due to mX
+		normZ = normZ1 + normZ2 + normZ3;	// Normal stress Total
 		
-		$("#NormalZ").html("Normal Stress in Z: <br>Due to Axial Force: " + normZ1.toFixed(3) + " " + UnitChoice[text2].pressureText
-			+ "<br>Due to moment caused by force in Y and moment at end: " + normZ2.toFixed(3) + " " + UnitChoice[text2].pressureText
-			+ "<br>Total Normal Stress in Z: " + normZ.toFixed(3) + " " + UnitChoice[text2].pressureText);
+		$("#NormalZaxial").html(normZ1.toFixed(3) + " " + UnitChoice[text2].pressureText);
+		$("#NormalZbending1").html(normZ2.toFixed(3) + " " + UnitChoice[text2].pressureText);
+		$("#NormalZbending2").html(normZ3.toFixed(3) + " " + UnitChoice[text2].pressureText);
+		$("#NormalZtot").html(normZ.toFixed(3) + " " + UnitChoice[text2].pressureText);
 		
-		
+
 		// Shear stress
 		// fX and fY contribute, and torsion (mZ)
 		
