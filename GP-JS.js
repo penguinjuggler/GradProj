685dfdc990fc488785fbf92f3e1d0ba11d98e606
Jacob M <penguinjuggler@gmail.com>
2020-07-31 00:23:25 -0700

    Stress and Strain Explanation Content updated. Bear Pics added

--- GP-JS.js
+++ GP-JS.js
@@ -237,7 +237,18 @@ $(document).ready(function() {
 	});
 });
 
-
+// Tool Tip Code
+jQuery.noConflict();
+jQuery(function() {
+jQuery(".refbody").hide();
+jQuery(".refnum").click(function(event) {
+  jQuery(this.nextSibling).toggle();
+  event.stopPropagation();
+});
+jQuery("body").click(function(event) {
+  jQuery(".refbody").hide();
+});
+});
 	
 
 
